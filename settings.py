1a9d7b3ed2fae0f89ff890c97fe36e2ad993e357
zacharymeyerdev <100534924+zacharymeyerdev@users.noreply.github.com>
2024-03-25 10:42:25 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,11 @@
+# Writing the Python code for 'settings.py' of the platformer game
 
+settings_py_code = """
+# Game settings
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Colors
+BG_COLOR = (135, 206, 250)  # Sky blue background
+"""
