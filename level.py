2bdbaa26bb84d62f9b9c7a63f3048d968cc54850
zacharymeyerdev <100534924+zacharymeyerdev@users.noreply.github.com>
2024-03-25 10:42:41 -0600

    Update level.py

--- level.py
+++ level.py
@@ -1,1 +1,15 @@
+# Writing the Python code for 'level.py' of the platformer game
 
+level_py_code = """
+import pygame
+from player import Player
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.player = Player()
+
+    def run(self):
+        self.display_surface.blit(self.player.image, self.player.rect)
+        self.player.update()
+"""
